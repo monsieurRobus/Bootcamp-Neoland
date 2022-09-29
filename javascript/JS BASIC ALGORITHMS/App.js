dd5a231de99f4b9deff12c75b7b18fafbf0ed2e8
cdperez <cdperez.85@gmail.com>
2022-09-29 19:54:14 +0200

    Iteracion#3

--- javascript/JS BASIC ALGORITHMS/App.js
+++ javascript/JS BASIC ALGORITHMS/App.js
@@ -10,3 +10,44 @@ z = h+y;
 const character = {name: 'Jack Sparrow', age: 10};
 character.age = 25;
 
+// 1.2
+firstName = 'Jon'; 
+lastName = 'Snow'; 
+age = 24; 
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.`)
+
+
+// 1.3
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+console.log(toy1.price + toy2.price);
+
+// 1.4
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = 25000;
+car1.finalPrice+=globalBasePrice;
+car2.finalPrice+=globalBasePrice;
+
+
+// Iteración 3
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+console.log(10*5);
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+console.log(10/2);
+// 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+console.log(10%5);
+// 1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+// teniendo dos variables p = 10 y j = 5.
+let p = 10;
+let j = 5;
+let o = p + j;
+
+// 1.5 Usa el correcto operador de asignación que resultará en i = 50,
+// teniendo dos variables c = 10 y m = 5.
+let c = 10;
+let m = 5;
+let i = c * j;
