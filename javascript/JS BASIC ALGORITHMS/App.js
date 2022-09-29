6d504700778b2c27c7033eeab8eb4864c6bb6d51
cdperez <cdperez.85@gmail.com>
2022-09-29 19:54:48 +0200

    Iteracion#3

--- javascript/JS BASIC ALGORITHMS/App.js
+++ javascript/JS BASIC ALGORITHMS/App.js
@@ -51,3 +51,47 @@ let o = p + j;
 let c = 10;
 let m = 5;
 let i = c * j;
+
+// Iteración 4
+// 1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers[0]);
+
+avengers[0]="IRONMAN";
+console.log(avengers.length);
+
+
+// Iteración 5
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+// ejemplo
+if(number1 === 10){
+    console.log('number1 es estrictamente igual a 10')
+}
+
+if (number2/number1 === 2) {
+    console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1!=number2) {
+    console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3!=number1) {
+    console.log("number3 es distinto number1");
+}
+
+if (number3*5===number1) {
+    console.log("number3 por 5 es igual a number1");
+}
+
+if (number3*5 === number1 && number1*2 === number2) {
+    console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+if (number2/2 === number1 || number1/5 === number3) {
+    console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
