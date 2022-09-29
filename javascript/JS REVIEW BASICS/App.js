b4e44c50719d2c819e713afefc52f399a7fe80a4
cdperez <cdperez.85@gmail.com>
2022-09-30 01:12:39 +0200

    Iteracion#2

--- javascript/JS REVIEW BASICS/App.js
+++ javascript/JS REVIEW BASICS/App.js
@@ -28,3 +28,47 @@ for(peli of movies)
 let categorias2 = new Set(catAux);
 
 console.log(categorias2)
+
+
+// Iteracion #2
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+let media = 0;
+let sonidos = 0;
+for (usuario of users)  
+    for (sonido in usuario.favoritesSounds)
+        media=usuario.favoritesSounds[sonido].volume;
+        sonidos+=1;
+
+media=media/sonidos;
+console.log(media)
