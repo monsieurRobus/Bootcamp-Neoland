4ff71609fbd3439122af09974aecc13c37e5060e
cdperez <cdperez.85@gmail.com>
2022-09-29 20:44:28 +0200

    Iteracion#5

--- javascript/JS LOOPS/App.js
+++ javascript/JS LOOPS/App.js
@@ -63,3 +63,19 @@ const alien = {
 
 for (propiedad in alien) 
     console.log(`${propiedad}: ${alien[propiedad]}`)
+
+// Iteracion #5
+const placesToTravel2 = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
+
+    for (let i=0; i<placesToTravel2.length;i++)
+        if (placesToTravel2[i].id == 11 || placesToTravel2[i].id == 40)
+            placesToTravel2.splice(i,1)
+        
+console.log(placesToTravel2)
+
+
+
+
+
+
+
