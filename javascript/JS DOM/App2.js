e42a6e4ba4fc69cf9f2df1ea5a2c345f33b605be
cdperez <cdperez.85@gmail.com>
2022-10-01 02:29:38 +0200

    Iteracion#2 a medio

---
+++ javascript/JS DOM/App2.js
@@ -0,0 +1,52 @@
+// Iteracion 2
+// 2.1 Inserta dinamicamente en un html un div vacio con javascript.
+let divVacio = document.createElement("div")
+document.body.appendChild(divVacio)
+
+// 2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.
+let divP = document.createElement("div")
+let p = document.createElement("p")
+divP.appendChild(p)
+document.body.appendChild(divP)
+
+// 2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
+let div6p = document.createElement("div")
+for (let i=0;i<6;i++)
+    {
+        div6p.appendChild(document.createElement("p"))
+    }
+document.body.appendChild(div6p)
+
+
+// 2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
+let divp2 = document.createElement("div")
+let p2 = document.createElement("p") 
+let texto = document.createTextNode("Soy dinámico!") // Duda, que es mejor, createTextNode o innerHTML?
+p2.appendChild(texto)
+divp2.appendChild(p2)
+document.body.appendChild(divp2)
+
+// 2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
+let h2 = document.querySelector(".fn-insert-here")
+let texto2 = document.createTextNode("Wubba Lubba dub dub")
+h2.appendChild(texto2)
+
+// 2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+
+let ul = document.createElement("ul")
+let li;
+for (elemento of apps)
+    {   
+        li = document.createElement("li")
+        li.innerHTML=elemento;
+        ul.appendChild(li)
+    }
+document.body.appendChild(ul)
+// 2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
+
+
+// 2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
+// 	Recuerda que no solo puedes insertar elementos con .appendChild.
+
+// 2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here
