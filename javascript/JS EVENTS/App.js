4e8eeaad055e21eaab66a80428b75925271f4fef
cdperez <cdperez.85@gmail.com>
2022-10-04 01:13:38 +0200

    Iteracion #1 a medio

---
+++ javascript/JS EVENTS/App.js
@@ -0,0 +1,13 @@
+// 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
+// evento click que ejecute un console log con la información del evento del click
+let button = document.querySelector("#btnToClick")
+let evento = e => console.log(e)
+
+button.addEventListener("click",evento)
+
+
+
+
+// 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
+
+// 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
