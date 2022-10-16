2727275309e40a152ef45cf12a9f2d90cd391002
cdperez <cdperez.85@gmail.com>
2022-10-16 23:07:18 +0200

    Iteracción 2 ES6

--- javascript/JS ES6/app.js
+++ javascript/JS ES6/app.js
@@ -20,26 +20,44 @@ console.log(funcionFlecha(3,2))
 const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
 
 const {title,gender,year} = game
-console.log(title)
-console.log(gender)
-console.log(year)
 
 // 2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
 // llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
 // imprimelo por consola.
 
-// const fruits = ['Banana', 'Strawberry', 'Orange'];
+const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+const [fruit1,fruit2,fruit3] = fruits;
+console.log(`${fruit1} ${fruit2} ${fruit3}`)
 
 // 2.3 En base al siguiente javascript, usa destructuring para crear 2 
 // variables igualandolo a la función e imprimiendolo por consola.
 
-// const animalFunction = () => {
-//     return {name: 'Bengal Tiger', race: 'Tiger'}
-// };
+
+
+const animalFunction = () => {
+    return {name: 'Bengal Tiger', race: 'Tiger'}
+};
+
+const {name,race} = animalFunction()
+console.log(`${name} y su raza es ${race}`)
+
 
 // 2.4 En base al siguiente javascript, usa destructuring para crear las 
 // variables name y itv con sus respectivos valores. Posteriormente crea 
 // 3 variables usando igualmente el destructuring para cada uno de los años 
 // y comprueba que todo esta bien imprimiendolo.
 
-// const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+const car = {name1: 'Mazda 6', itv1: [2015, 2011, 2020] }
+
+const {name1,itv1} = car
+
+// Para arreglar el orden de los años, ordenamos los mismos en el array
+
+itv1.sort()
+
+// Y ahora deconstruimos el array
+const [anio1,anio2,anio3] = itv1
+
+
+console.log(`${anio1} ${anio2} ${anio3}`)
