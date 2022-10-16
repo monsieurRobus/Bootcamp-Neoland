1d50f20360813a58b1d16f5943825dad338c26ce
cdperez <cdperez.85@gmail.com>
2022-10-16 23:20:17 +0200

    Iteración #3 ES6

--- javascript/JS ES6/app.js
+++ javascript/JS ES6/app.js
@@ -13,6 +13,8 @@ console.log(funcionFlecha())
 console.log(funcionFlecha(3))
 console.log(funcionFlecha(3,2))
 
+
+// #ITERACION 2
 // 2.1 En base al siguiente javascript, crea variables en base a las propiedades 
 // del objeto usando object destructuring e imprimelas por consola. Cuidado, 
 // no hace falta hacer destructuring del array, solo del objeto.
@@ -61,3 +63,37 @@ const [anio1,anio2,anio3] = itv1
 
 
 console.log(`${anio1} ${anio2} ${anio3}`)
+
+
+// #ITERACION 3
+
+// 3.1 Dado el siguiente array, crea una copia usando spread operators.
+const pointsList = [32, 54, 21, 64, 75, 43]
+
+const copiaList = [...pointsList]
+
+// 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+
+const copiaToy = {...toy}
+
+// 3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
+// spread operatos.
+const pointsList1 = [32, 54, 21, 64, 75, 43];
+const pointsList2 = [54,87,99,65,32];
+
+const pointLists = [...pointsList1,...pointsList2]
+
+// 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
+// con spread operators.
+const toy1 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+
+const toyFinal = {...toy1,toyUpdate};
+
+// 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
+// pero sin editar el array inicial. De nuevo, usando spread operatos.
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+const copiaColors= [...colors]
+copiaColors.splice(1,1)
+console.log(`${colors} ${copiaColors}`)
