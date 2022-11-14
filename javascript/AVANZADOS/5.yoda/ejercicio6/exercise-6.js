a736323d513a126b1fae6a465e5abf34fcb7da2b
cdperez <cdperez.85@gmail.com>
2022-11-14 01:34:47 +0100

    Ejercicio 6 ida de perola total

--- javascript/AVANZADOS/5.yoda/ejercicio6/exercise-6.js
+++ javascript/AVANZADOS/5.yoda/ejercicio6/exercise-6.js
@@ -1,18 +1,26 @@
 const characterSelect = document.querySelector("[data-function='characters']")
 const urlCharacters = 'http://localhost:3000/characters'
-
+const botonJugar = document.querySelector("#comienza-juego")
+const portadaJuego = document.querySelector("#portada")
 const getCharacters = async(url) => {
 
-    return await fetch(url).then(res => res.json())     
+    return await fetch(url).then(res => res.json()) 
+    .then(res=>pintaCharacters(res))
 
 }
 
 const pintaCharacters = async(personajes) => {
 
-    for (personaje of personajes)
+    for await (personaje of personajes)
         {
+            console.log(personaje.avatar)
             const div = document.createElement("div")
-            div.classList.add('character-choice')
+            div.innerHTML=`<h1>${personaje.name}</h1><div class="stats"><span>⚔️${personaje.critic}</span><span>🛡️${personaje.defense}</span><span>💓${personaje.vitality}</span></div>`
+            div.classList.add('c-characters__item')
+            div.style.backgroundImage=`url("${personaje.avatar}")`
+            div.style.backgroundSize="cover";
+            characterSelect.appendChild(div)
+
         }
 
 }
@@ -21,4 +29,8 @@ const pintaCharacters = async(personajes) => {
 
 let personajes = getCharacters(urlCharacters)
 
-console.log(personajes)
+
+
+botonJugar.addEventListener('click',(b) => {
+    portadaJuego.classList.remove('inicio')    
+})
