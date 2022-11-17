cc258a21fc8b6b46131791ccba6a5d54e6300f45
Carlos <cdperez.85@gmail.com>
2022-11-17 19:13:24 +0100

    vite

---
+++ javascript/AVANZADOS/proyectos-vite/color-picker/main.js
@@ -0,0 +1,61 @@
+import './style.css'
+
+const COLOR_PALETTE = {
+  '#251101': 'Bistre',
+  '#470024': 'Tyrian Purple',
+  '#5B1865': 'Midnight',
+  '#2C5784': 'Bdazzled Blue',
+  '#5688C7': 'Glaucous'
+}
+
+const colorPicker = document.querySelector("#color-picker")
+const colorTitle = document.querySelector("#color-name")
+const inputColor = document.querySelector('#input-color')
+
+
+
+const addOptionsToColorPicker = () => {
+  
+  Object.keys(COLOR_PALETTE).forEach((color) => {
+
+    const opt = document.createElement("option") // creamos elemento option
+    opt.value = color                           // asignamos a cada opcion el valor del elemento color
+    opt.innerHTML=COLOR_PALETTE[color]          // el contenido del elemento opcion
+  
+    colorPicker.appendChild(opt)                // Colocamos cada OPT dentro del elemento colorPicker
+  
+  })
+
+}
+
+const addEventListenersToColorPicker = () => {
+
+  colorPicker.addEventListener("change", e => {
+
+    document.body.style.backgroundColor=e.target.value
+    const colorTitleContent = `${COLOR_PALETTE[e.target.value]} - ${e.target.value}` 
+    
+    colorTitle.innerHTML = COLOR_PALETTE[e.target.value] ? colorTitleContent : '-'
+  })
+
+
+
+  const actualizaInputColor = (e) => {
+
+      document.body.style.backgroundColor=e.target.value
+      const colorTitleContent = `${COLOR_PALETTE[e.target.value]} - ${e.target.value}`
+      console.log(COLOR_PALETTE[e.target.value]) 
+      
+      inputColor.innerHTML = COLOR_PALETTE[e.target.value] ? colorTitleContent : `${e.target.value}`
+    
+  }
+
+  
+  inputColor.addEventListener("change", actualizaInputColor , false)
+  inputColor.addEventListener("input", actualizaInputColor , false)
+
+}
+
+addOptionsToColorPicker()
+addEventListenersToColorPicker()
+
