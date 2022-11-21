f3592b6f4f6249dbdcfcad15e0095a9950075312
cdperez <cdperez.85@gmail.com>
2022-11-22 00:58:38 +0100

    Dando vueltas sobte mi mismo ejercicio 6

--- javascript/AVANZADOS/5.yoda/ejercicio6/exercise-6.js
+++ javascript/AVANZADOS/5.yoda/ejercicio6/exercise-6.js
@@ -2,10 +2,20 @@ const characterSelect = document.querySelector("[data-function='characters']")
 const urlCharacters = 'http://localhost:3000/characters'
 const botonJugar = document.querySelector("#comienza-juego")
 const portadaJuego = document.querySelector("#portada")
-const getCharacters = async(url) => {
 
-    return await fetch(url).then(res => res.json()) 
-    .then(res=>pintaCharacters(res))
+
+
+let jugador1 = {};
+let jugador2 = {};
+let personajes;
+
+const getCharacters = async(url) => {
+    let listaPersonajes;
+    await fetch(url).then(res => res.json()) 
+        .then(res => listaPersonajes=res)
+        .then(res=>pintaCharacters(res))
+
+    return listaPersonajes
 
 }
 
@@ -13,13 +23,16 @@ const pintaCharacters = async(personajes) => {
 
     for await (personaje of personajes)
         {
+            
             console.log(personaje.avatar)
             const div = document.createElement("div")
+            div.id=`${personaje.name}`
             div.innerHTML=`<h1>${personaje.name}</h1><div class="stats"><span>⚔️${personaje.critic}</span><span>🛡️${personaje.defense}</span><span>💓${personaje.vitality}</span></div>`
             div.classList.add('c-characters__item')
             div.style.backgroundImage=`url("${personaje.avatar}")`
             div.style.backgroundSize="cover";
             characterSelect.appendChild(div)
+            div.addEventListener('click',seleccionaPersonaje)
 
         }
 
@@ -27,10 +40,30 @@ const pintaCharacters = async(personajes) => {
 
 
 
-let personajes = getCharacters(urlCharacters)
-
-
+personajes =  getCharacters(urlCharacters)
 
 botonJugar.addEventListener('click',(b) => {
-    portadaJuego.classList.remove('inicio')    
+    portadaJuego.classList.remove('inicio')   
+    portadaJuego.classList.add('modal-ocultar') 
 })
+
+
+const seleccionaPersonaje = (e) => {
+
+    console.log(personajes)
+    const jugador = e.target.id
+    if(jugador1 === undefined)
+        {
+            pintaJugador(jugador,1)
+        }
+    else    
+        {
+            pintaJugador(jugador,2)
+        }
+}
+
+const pintaJugador = (jugador,numero) => {
+
+
+
+}
