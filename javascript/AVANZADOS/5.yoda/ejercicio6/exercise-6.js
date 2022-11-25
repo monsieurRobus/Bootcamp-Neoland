f8e20ec4b3305b1117efc0bf0372741acb7ba547
cdperez <cdperez.85@gmail.com>
2022-11-25 01:41:15 +0100

    fantasy battle to guapa

--- javascript/AVANZADOS/5.yoda/ejercicio6/exercise-6.js
+++ javascript/AVANZADOS/5.yoda/ejercicio6/exercise-6.js
@@ -2,28 +2,26 @@ const characterSelect = document.querySelector("[data-function='characters']")
 const urlCharacters = 'http://localhost:3000/characters'
 const botonJugar = document.querySelector("#comienza-juego")
 const portadaJuego = document.querySelector("#portada")
-
-
+const jugarSFX = new Audio('./public/sfx/pickupCoin.wav');
+const centroJuego = document.querySelector('#centro')
 
 let jugador1;
 let jugador2;
-let personajes;
+let personajes = [];
 
 const getCharacters = async(url) => {
     let listaPersonajes;
     await fetch(url).then(res => res.json()) 
-        .then(res=>listaPersonajes=res)
+        .then(res=>personajes=res)
         .then(res=>pintaCharacters(res))
 
-    return listaPersonajes
 }
 
-const pintaCharacters = async(personajes) => {
+const pintaCharacters = (personajes) => {
 
-    for await (personaje of personajes)
+    for  (personaje of personajes)
         {
             
-            console.log(personaje.avatar)
             const div = document.createElement("div")
             div.id=`${personaje.name}`
             div.innerHTML=`<h1>${personaje.name}</h1><div class="stats"><span>⚔️${personaje.critic}</span><span>🛡️${personaje.defense}</span><span>💓${personaje.vitality}</span></div>`
@@ -34,14 +32,12 @@ const pintaCharacters = async(personajes) => {
             div.addEventListener('click',seleccionaPersonaje)
 
         }
-
 }
 
-
-
-personajes =  getCharacters(urlCharacters)
+getCharacters(urlCharacters)
 
 botonJugar.addEventListener('click',(b) => {
+    jugarSFX.play()
     portadaJuego.classList.remove('inicio')   
     portadaJuego.classList.add('modal-ocultar') 
 })
@@ -49,21 +45,110 @@ botonJugar.addEventListener('click',(b) => {
 
 const seleccionaPersonaje = (e) => {
 
-    console.log(personajes)
     const jugador = e.target.id
     if(jugador1 === undefined)
         {
-            pintaJugador(jugador,1)
-            console.log(personajes.filter((personaje)=>personaje.name === jugador))
+            e.target.classList.add('seleccionado-jugador1')
+            jugador1 =  personajes.filter( personaje => personaje.name === e.target.id )
+            pintaJugador(jugador1,1)
         }
     else    
         {
-            pintaJugador(jugador,2)
+            e.target.classList.add('seleccionado-jugador2')
+            jugador2 =  personajes.filter( personaje => personaje.name === e.target.id )
+            pintaJugador(jugador2,2)
         }
+
+        if(jugador1 && jugador2 !== undefined)
+            setTimeout(()=>{
+                
+                for (hijo   of characterSelect.children)
+                    {
+                        hijo.classList.remove('seleccionado-jugador1')
+                        hijo.classList.remove('seleccionado-jugador2')
+                        characterSelect.classList.add('desaparece')
+                    }
+                
+
+            },1500)
 }
 
-const pintaJugador = (jugador,numero) => {
+const pintaJugador = async(jugador,numero) => {
+    let divJugador;
+        switch(numero){
+            case 1:                 
+                divJugador = document.querySelector("#jugador-1")
+                
+                divJugador.innerHTML=`
+                    <figure class="tarjeta entrada">
+                        <div class="tarjeta-contenedor">
+                            <div style="background-image: url('${jugador[0].avatar}'); background-size:cover" class="tarjeta-frente">
+                                <div class="stats"><span>⚔️${personaje.critic}</span><span>🛡️${personaje.defense}</span><span>💓${personaje.vitality}</span></div>
+                            </div>
+                            <div class="tarjeta-espalda1">
+                                <img class="tarjeta-trasera-logo" src="./img/trasera-tarjeta.png" />
+                                <h1>${jugador[0].name}</h1>                                    
+                            </div>
+                        </div>
+                    </figure>
+                `
+                tiradasDados(jugador[0].damage) // Comprobamos que funciona
+                divJugador.style.backgroundSize='cover'
+            break;
+
+            case 2:                
+                divJugador = document.querySelector("#jugador-2")
+                
+                divJugador.innerHTML=`
+                    <figure class="tarjeta entrada">
+                        <div class="tarjeta-contenedor">
+                            <div style="background-image: url('${jugador[0].avatar}'); background-size:cover" class="tarjeta-frente">
+                                <div class="stats"><span>⚔️${personaje.critic}</span><span>🛡️${personaje.defense}</span><span>💓${personaje.vitality}</span></div>
+                            </div>
+                            <div class="tarjeta-espalda2">
+                                <img class="tarjeta-trasera-logo" src="./img/trasera-tarjeta.png" />
+                                <h1>${jugador[0].name}</h1>                                    
+                            </div>
+                        </div>
+                    </figure>
+                `
+                tiradasDados(jugador[0].damage) // Comprobamos que funciona
+                divJugador.style.backgroundSize='cover'
+            break;
+        }
+
+}
 
 
+const tiradasDados = (tiradas) => {
+centroJuego.innerHTML=''
+let danioTotal=0;
+        
+        for (tirada of tiradas) {
+            tirada=tirada.split('d')
+            
+            let [dados,danioMax] = tirada
+            dados= parseInt(dados)
+            
+            danioMax = parseInt(danioMax)
+            // Bucle de tiradas de dado
+                for (var i=0;i<dados;i++){
+                    setTimeout(()=>{
+                        let danio = Math.floor(Math.random()*danioMax)+1
+                        danioTotal+=danio
+                        centroJuego.innerHTML+=`<span>${danio}</span>`;
+                    },250*i)
+        }
+
+        
+        
+
+
+}
+        // dibujamos el daño final
+        setTimeout(()=>{
+                    
+            centroJuego.innerHTML+=`<span class="danio-total">${danioTotal}</span>`;
+        },1000*tiradas.length)
 
 }
