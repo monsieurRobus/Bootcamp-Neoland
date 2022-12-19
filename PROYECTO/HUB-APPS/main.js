ef0cbb35d52d36b38552286afd7789bc3ff3fe58
cdperez <cdperez.85@gmail.com>
2022-12-20 00:24:33 +0100

    Login practicamente acabado

--- PROYECTO/HUB-APPS/main.js
+++ PROYECTO/HUB-APPS/main.js
@@ -3,20 +3,24 @@ import { Header } from './components/header'
 import { changeBackground } from './utils/utils'
 import { Footer } from './components/footer'
 import { Hub } from './pages/Hub/Hub'
+import { Login } from './pages/Login/Login'
+
 
 const pageStorage = window.localStorage;
+let user = pageStorage.user
 
 
 document.querySelector('#app').innerHTML = `
   ${Header()}
   <main>
+    ${Login()}
   </main>
   ${Footer()}
 `
 
 // AQUI METEMOS TB EVENT LISTENERS Y ROUTEO
 
-export const addHeaderEventListeners = () => {
+  const addHeaderEventListeners = () => {
   const header = document.querySelector("header")
   const buttons = header.querySelectorAll("button")
 
@@ -27,21 +31,38 @@ export const addHeaderEventListeners = () => {
             button.addEventListener('click', ()=>{changeBackground()})
           break;
         case 'logout':
-          button.addEventListener('click', ()=>{alert("LOGAUT!")})
+            button.addEventListener('click', ()=>{alert("LOGAUT!")})
           break;
       }
     }
 
 }
 
-const itemSelector = (navigate) => {
+const addLoginEventListeners = () => {
 
-switch (navigate) {
-  case undefined: 
-  break
-}
-
+  const main = document.querySelector("main")
+  const button = main.querySelectorAll("button")
+  button[0].addEventListener('click', () => userLogin())
 
 }
+
+const userLogin = () => {
+
+  const userLogin = document.querySelector('#nameLogin')
+  userLogin.value == '' ? invalidUsername() :  pageStorage.setItem('user',userLogin.value)
+
+}
+
+const invalidUsername = () => {
+
+  const main = document.querySelector("main")
+  const message = document.createElement('h2').innerHTML=(document.createTextNode("Usuario no válido"))
+console.log(message)
+  main.appendChild(message)
+  
+
+}
+
 
 addHeaderEventListeners()
+addLoginEventListeners()
---
+++ PROYECTO/HUB-APPS/pages/Login/Login.js
@@ -0,0 +1,9 @@
+import './Login.css'
+
+export const Login = () => `
+<h2>¡Hola! uwu</h2>
+<h3>¿Cómo te llamas?🤔</h3>
+<div>
+    <input id="nameLogin"></input><button>➡️</button>
+</div>`
+
