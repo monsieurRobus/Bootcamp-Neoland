1aa3b1c861ec90a07b5dbfa88b919bc1763b018b
Carlos Díaz <cdiazperez@ybvr.com>
2023-02-27 20:22:26 +0100

    Initial prototype

---
+++ react/viewSetlistPrototype/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename={'/'}>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />}/>
+        </Route>
+      </Routes>
+    <App />
+    
+    
+    </BrowserRouter>
+
+    
+  </React.StrictMode>,
+)
