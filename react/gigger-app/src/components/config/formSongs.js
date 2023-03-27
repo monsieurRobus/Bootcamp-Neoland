cfa2601b973f771dfc2ab8ca5795a471d3e8b0ce
cdperez <cdperez.85@gmail.com>
2023-03-27 19:37:30 +0200

    Sync

---
+++ react/gigger-app/src/components/config/formSongs.js
@@ -0,0 +1,36 @@
+export const formSongs = [
+
+    {
+        name: "Song Name",
+        componentType: "input",
+        type: "text",
+        label: "Song Name",
+        config: {}   
+    },
+    {
+        name: "Artist",
+        componentType: "input",
+        type: "text",
+        label: "Artist",
+        config: {}   
+    },
+    {
+        name: "Duration",
+        componentType: "input",
+        type: "number",
+        label: "Duration",
+        config: {}   
+    },
+    {
+        name: "Lyrics",
+        componentType: "input",
+        label: "Lyrics",
+        type: "textarea",
+        config: {
+            rows: 10,
+            cols: 30
+        }   
+
+    }
+
+]
---
+++ react/gigger-app/src/utils/ButtonComponent/ButtonComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ButtonComponent = (props) => {
+    const {text} = props 
+  return (
+    <button>{text}</button>
+  )
+}
+
+export default ButtonComponent
